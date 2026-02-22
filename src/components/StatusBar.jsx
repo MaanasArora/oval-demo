65d5290110fc58f659885c98f0216bac29e73ab6
Maanas Arora <maanasarora23@gmail.com>
2026-02-22 10:32:38 -0500

    Refactor StatusBar component to wrap author text in a span for improved styling

--- src/components/StatusBar.jsx
+++ src/components/StatusBar.jsx
@@ -1,12 +1,12 @@
 export default function StatusBar() {
   return (
     <div className="bg-white border-t px-4 py-2 text-sm flex items-center">
-      Created by Maanas Arora &nbsp;
+      <span className="mr-4">Created by Maanas Arora</span>
       <div className="flex items-center">
         <img
           src="GitHub_Invertocat_Black.svg"
           alt="GitHub"
-          className="inline w-5 h-5"
+          className="inline w-5 h-5 mr-2"
         />
         <a
           href="https://github.com/MaanasArora/oval-demo"
